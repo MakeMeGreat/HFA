e1ba6a0de0ca206bca053ebbac4b7c4904ad31be
Azamat <zamametshin@gmail.com>
2023-11-13 11:54:03 +0300

    +exp navigation toolbar and bottomnavigationbar

---
+++ app/src/main/java/ru/easycode/hfa_first/SentItemsFragment.kt
@@ -0,0 +1,17 @@
+package ru.easycode.hfa_first
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SentItemsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sent_items, container, false)
+    }
+}
