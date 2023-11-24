03473dac1c1051f740243e49b2e05c3d13e8ad2a
Azamat <zamametshin@gmail.com>
2023-11-24 14:29:32 +0300

    +exp RecyclerView adapter viewHolder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.room:room-ktx:2.6.0")
     implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     kapt("androidx.room:room-compiler:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/ru/easycode/hfa_first/TasksFragment.kt
+++ app/src/main/java/ru/easycode/hfa_first/TasksFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import ru.easycode.hfa_first.databinding.FragmentTasksBinding
 
@@ -30,6 +31,14 @@ class TasksFragment : Fragment() {
         ).get(TasksViewModel::class.java)// инстанс ВМ с аргументом в конструкторе
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter = TaskItemAdapter()
+        binding.tasksList.adapter = adapter
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
         return view
     }
 
