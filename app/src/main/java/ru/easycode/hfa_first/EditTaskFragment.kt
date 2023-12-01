b0a27eec33bfa06983d29b8d0559d961dd432a82
Azamat <zamametshin@gmail.com>
2023-12-01 15:19:16 +0300

    +exp make EditFragment update and delete task from database

--- app/src/main/java/ru/easycode/hfa_first/EditTaskFragment.kt
+++ app/src/main/java/ru/easycode/hfa_first/EditTaskFragment.kt
@@ -4,18 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import ru.easycode.hfa_first.databinding.FragmentEditTaskBinding
 
 class EditTaskFragment : Fragment() {
+    private var _binding: FragmentEditTaskBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_edit_task, container, false)
-        val textView = view.findViewById<TextView>(R.id.task_id)
+        _binding = FragmentEditTaskBinding.inflate(inflater, container, false)
+        val view = binding.root
         val taskId = EditTaskFragmentArgs.fromBundle(requireArguments()).taskId
-        textView.text = taskId.toString()
+        val application = requireNotNull(this.activity).application
+        val dao =
+            TaskDatabase.getInstance(application).taskDao //эта строка и выше нужны чтобы сделать VMFactory
+        val viewModelFactory = EditTaskViewModelFactory(taskId, dao)
+        val viewModel = ViewModelProvider(
+            this,
+            viewModelFactory
+        ).get(EditTaskViewModel::class.java)// с помощью factory создаем ВМ и получаем ссылку на него
+
+        binding.viewModel = viewModel // связываем с макетом
+        binding.lifecycleOwner = viewLifecycleOwner //даем макету взаимодействовать с LiveData
+
+        viewModel.navigateToList.observe(
+            viewLifecycleOwner,
+            Observer { navigate -> //если свойство вмки тру то переход к таск фрагменту
+                if (navigate) {
+                    view.findNavController().navigate(R.id.action_editTaskFragment_to_tasksFragment)
+                    viewModel.onNavigatedToList() //меняем свойство обратно на false
+                }
+            })
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
