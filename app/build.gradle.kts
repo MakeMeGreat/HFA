6b20c166a33745c49d8383b7654e894a6fa57f01
Azamat <zamametshin@gmail.com>
2023-12-07 17:47:19 +0300

    +exp add composable button and text in view using app (version with maket and composable)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ android {
 
     buildFeatures {
         dataBinding = true
+        compose = true
     }
 
     buildTypes {
@@ -36,6 +37,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -43,12 +48,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui-tooling:1.5.4")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/ru/easycode/hfa_first/ResultFragment.kt
+++ app/src/main/java/ru/easycode/hfa_first/ResultFragment.kt
@@ -4,6 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -19,7 +30,15 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        _binding = FragmentResultBinding.inflate(inflater, container, false).apply {
+            composeView.setContent {
+                MaterialTheme {
+                    Surface {
+                        view?.let { ResultFragmentContent(view = it, viewModel = viewModel) }
+                    }
+                }
+            }
+        }
         val view = binding.root
 
         val result = ResultFragmentArgs.fromBundle(requireArguments()).result
@@ -36,5 +55,34 @@ class ResultFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @Composable
+    fun ResultText(result: String) {
+        Text(
+            text = result,
+            fontSize = 28.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+
+    @Composable
+    fun NewGameButton(clicked: () -> Unit) {
+        Button(onClick = clicked) {
+            Text(text = "Start New Game")
+        }
+    }
+
+    @Composable
+    fun ResultFragmentContent(view: View, viewModel: ResultViewModel) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ResultText(viewModel.result)
+            NewGameButton {
+                view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
+            }
+        }
     }
 }
